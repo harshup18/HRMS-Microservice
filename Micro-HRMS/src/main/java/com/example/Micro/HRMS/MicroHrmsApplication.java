77b80b8778683991e4751d52fa928eeb429eae24
Harsh Upadhyay <31928299+harshup18@users.noreply.github.com>
2020-09-22 15:36:33 +0530

    Add files via upload

---
+++ Micro-HRMS/src/main/java/com/example/Micro/HRMS/MicroHrmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Micro.HRMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroHrmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroHrmsApplication.class, args);
+	}
+
+}
---
+++ Micro-HRMS/src/test/java/com/example/Micro/HRMS/MicroHrmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Micro.HRMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroHrmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
